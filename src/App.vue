1be740df3a7f121821c7fb796dc505fc9a388810
wenxuanjun <wendster@163.com>
2021-09-18 19:39:30 +0800

    New: First page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
+<style>
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.child-view {
+  position: absolute;
+  transition: all .5s cubic-bezier(.5, 0, .1, 1);
+}
+</style>
+
 <template>
   <v-app>
-    <v-app-bar app></v-app-bar>
-    <v-main></v-main>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list nav dense>
+        <v-list-item v-for="item in list_items" :key="item.title" :to="item.link">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-main>
+      <transition name="fade">
+        <router-view class="child-view"></router-view>
+      </transition>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import { mdiViewDashboard } from "@mdi/js";
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      list_items: [
+        { title: "Dashboard", icon: mdiViewDashboard, link: "/" }
+      ],
+      drawer: true
+    }
+  }
 };
 </script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-card-text>
+            <v-skeleton-loader type="date-picker"></v-skeleton-loader>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="8">
+        <v-card>
+          <v-card-text>
+            <v-skeleton-loader type="card-avatar, article"></v-skeleton-loader>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+};
+</script>
